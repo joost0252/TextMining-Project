6e7a477913fe1f8f0a510de1860b4602ed107cfe
katia-voltz <93271091+katia-voltz@users.noreply.github.com>
2022-11-17 11:42:32 +0100

    Update setup.R

--- scripts/setup.R
+++ scripts/setup.R
@@ -7,6 +7,7 @@ packages <- c(
   "rvest", # for webscraping 
   "tidyverse", "readxl", #for wrangling
   "kableExtra" # for a nice table 
+  ,"tidytext","wordcloud","lexicon","flextable","quanteda","quanteda.textstats","tm","ggrepel"
 )
 
 purrr::walk(packages, library, character.only = TRUE)
