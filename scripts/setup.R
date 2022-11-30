2267f01677930c09a439f2430721d42d8fc42f15
Elodie <79649046+Elodie-kwan@users.noreply.github.com>
2022-11-30 20:25:05 +0100

    analysis

--- scripts/setup.R
+++ scripts/setup.R
@@ -5,11 +5,12 @@
 # load the required packages
 packages <- c(
   "rvest", # for webscraping 
-  "tidyverse", "readxl", #for wrangling
+  "tidyverse", "readxl", # for wrangling
   "kableExtra", # for a nice table 
   "readr", "quanteda", "quanteda.textplots", "quanteda.textstats",
   "tidytext","wordcloud","lexicon","flextable","tm","ggrepel", 
-  "dplyr", "ggplot2", "broom", "ggwordcloud", "igraph", "udpipe"
+  "dplyr", "ggplot2", "broom", "ggwordcloud", "igraph", "udpipe",
+  "sentimentr" # for sentiment analysis 
 )
 
 purrr::walk(packages, library, character.only = TRUE)
