d9ec1dd620b046294637319f3e513614abebe467
nbidet <32996297+nbidet@users.noreply.github.com>
2022-12-03 12:20:38 +0100

    Update setup.R

--- scripts/setup.R
+++ scripts/setup.R
@@ -10,7 +10,10 @@ packages <- c(
   "readr", "quanteda", "quanteda.textplots", "quanteda.textstats",
   "tidytext","wordcloud","lexicon","flextable","tm","ggrepel", 
   "dplyr", "ggplot2", "broom", "ggwordcloud", "igraph", "udpipe",
-  "sentimentr" # for sentiment analysis 
+  "sentimentr", # for sentiment analysis 
+  "reshape2", # for similarities
+  "topicmodels", "topicdoc", "seededlda", "quanteda.textmodels" # for LDA, LSA, Topic Modelling
 )
 
 purrr::walk(packages, library, character.only = TRUE)
+
